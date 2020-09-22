caccccb148b22cea00734312ddeebe4606e508fb
ANI3792 <ary3792@gmail.com>
2020-09-22 13:37:44 +0530

    commit from IJ2

--- mav-ani1/src/main/java/com/edureka/maven1/App.java
+++ mav-ani1/src/main/java/com/edureka/maven1/App.java
@@ -4,10 +4,12 @@ package com.edureka.maven1;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    public static void main( String [] args )
     {
+
         System.out.println( "Hello World!" );
+        System.out.println("From IJ");
     }
 }
